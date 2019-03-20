8c2046c53fb863494fe5254e4820778c59dad6ea
andy-jenkins <ajenkins970@gmail.com>
2019-03-20 17:20:45 +0000

    Add `node-sass` and implement `kebab-case`

--- src/component/app.test.js
+++ src/component/app.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
+import App from './app';
+
+it('successfully renders', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
